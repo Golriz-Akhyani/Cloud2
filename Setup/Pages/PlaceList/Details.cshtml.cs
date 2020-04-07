c7ef2aeb290e8937fcf5bd2ff03cc89ec9bcd3a8
Chloe-cj <57932789+Chloe-cj@users.noreply.github.com>
2020-04-07 10:43:47 -0700

    listing page

---
+++ Setup/Pages/PlaceList/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Setup.Data;
+using Setup.Models;
+
+namespace Setup.Pages.PlaceList
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Setup.Data.SetupContext _context;
+
+        public DetailsModel(Setup.Data.SetupContext context)
+        {
+            _context = context;
+        }
+
+        public Place Place { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Place = await _context.Place.FirstOrDefaultAsync(m => m.PlaceID == id);
+
+            if (Place == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Setup/Pages/PlaceList/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Setup.Data;
+using Setup.Models;
+
+namespace Setup.Pages.PlaceList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Setup.Data.SetupContext _context;
+
+        public IndexModel(Setup.Data.SetupContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Place> Place { get;set; }
+        public string CurrentFilter { get; set; }
+        public async Task OnGetAsync(string searchstring)
+        {
+            IQueryable<Place> placeIQ = from s in _context.Place select s;
+            CurrentFilter = searchstring;
+            if (!String.IsNullOrEmpty(searchstring))
+            {
+                placeIQ = placeIQ.Where(s => s.PlaceName.Contains(searchstring));
+            }
+
+
+            Place = await placeIQ
+                .AsNoTracking().ToListAsync();
+        }
+    }
+}
